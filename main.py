6a7ebe8d86b774f10acdf896a23d84218e21fa5a
Yosr Frikha <yosr.frikha1@gmail.com>
2026-06-12 13:23:45 +0100

    requirements update and main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from api.routes.export import router as export_router
+from api.routes.rewrite import router as rewrite_router
 
 app = FastAPI()
 
 app.include_router(export_router)
+app.include_router(rewrite_router)
