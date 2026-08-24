1982e763447f208edc33b6d6208adf9922a8421e
Yosr Frikha <yosr.frikha1@gmail.com>
2026-08-24 14:49:25 +0100

    main and requirements update

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from api.routes.paraphrase import router as rewrite_router
 from api.routes.import_data import router as import_router
 from api.routes.measure_suggestion import router as measure_suggestion_router
 from api.routes.data_extraction import router as data_extraction_router
-from api.routes.findings_measures_suggestion import router as findings_measures_suggestion_router
 from api.routes.findings_suggestion import router as findings_suggestion_router
-from threatflow.api.assist import router as assist_router
-from threatflow.api.follow_up import router as followup_router
+from api.routes.findings_rag import router as findings_router
+from api.routes.measures_rag import router as measures_router
 
 app = FastAPI()
 
@@ -17,7 +16,7 @@ app.include_router(rewrite_router)
 app.include_router(import_router)
 app.include_router(measure_suggestion_router)
 app.include_router(data_extraction_router)
-app.include_router(findings_measures_suggestion_router)
 app.include_router(findings_suggestion_router)
-app.include_router(assist_router)
-app.include_router(followup_router)
+
+app.include_router(findings_router)
+app.include_router(measures_router)
